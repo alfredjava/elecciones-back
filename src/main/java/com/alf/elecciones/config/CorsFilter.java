e49a0856177caaea4734ff91fa9cc47a658cf6e1
alfredjava <alfredfis@gmail.com>
2026-01-16 23:45:46 -0500

    cors 2

--- src/main/java/com/alf/elecciones/config/CorsFilter.java
+++ src/main/java/com/alf/elecciones/config/CorsFilter.java
@@ -1,20 +1,25 @@
 package com.alf.elecciones.config;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
-@Provider // Esta anotación hace que Quarkus lo registre automáticamente
+@Provider
 public class CorsFilter implements ContainerResponseFilter {
+
+    // Inyectamos la variable. Si no existe, por defecto usa localhost
+    @ConfigProperty(name = "quarkus.http.cors.origins", defaultValue = "http://localhost:5173")
+    String allowedOrigin;
 
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", allowedOrigin);
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
