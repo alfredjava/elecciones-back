49bbc3f0c51427e955f6ef8ffb9438d1ab067390
alfredjava <alfredfis@gmail.com>
2026-01-21 16:35:11 -0500

    cor3

--- src/main/java/com/alf/elecciones/config/CorsFilter.java
+++ src/main/java/com/alf/elecciones/config/CorsFilter.java
@@ -1,25 +1,35 @@
-//package com.alf.elecciones.config;
-//
-//import org.eclipse.microprofile.config.inject.ConfigProperty;
-//import jakarta.ws.rs.container.ContainerRequestContext;
-//import jakarta.ws.rs.container.ContainerResponseContext;
-//import jakarta.ws.rs.container.ContainerResponseFilter;
-//import jakarta.ws.rs.ext.Provider;
-//import java.io.IOException;
-//
-//@Provider
-//public class CorsFilter implements ContainerResponseFilter {
-//
-//    // Inyectamos la variable. Si no existe, por defecto usa localhost
-//    @ConfigProperty(name = "quarkus.http.cors.origins", defaultValue = "http://localhost:5173")
-//    String allowedOrigin;
-//
-//    @Override
-//    public void filter(ContainerRequestContext requestContext,
-//                       ContainerResponseContext responseContext) throws IOException {
-//        responseContext.getHeaders().add("Access-Control-Allow-Origin", allowedOrigin);
-//        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-//        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-//        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-//    }
-//}
+package com.alf.elecciones.config;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @ConfigProperty(name = "quarkus.http.cors.origins", defaultValue = "http://localhost:5173")
+    String allowedOrigin;
+
+    @Override
+    public void filter(ContainerRequestContext request,
+                       ContainerResponseContext response) {
+
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", allowedOrigin);
+        response.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().putSingle(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization"
+        );
+        response.getHeaders().putSingle(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD"
+        );
+
+        // IMPORTANTE: manejar OPTIONS explícitamente
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(200);
+        }
+    }
+}
